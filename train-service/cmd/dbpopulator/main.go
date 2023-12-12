ee13b6a8badb1b714ec0249794a1fa89a86a7162
iskiy <vasyarosada@gmail.com>
2023-12-12 06:28:58 +0200

    merge

--- train-service/cmd/dbpopulator/main.go
+++ train-service/cmd/dbpopulator/main.go
@@ -1,5 +1,7 @@
 package main
 
-func main(){
-
-}
+//import "github.com/iskiy/railway-ticket-system/train-service/cmd/dbpopulator/populator"
+//
+//func main() {
+//	populator.DBPopulator{}
+//}
--- train-service/cmd/dbpopulator/populator/dbpopulator.go
+++ train-service/cmd/dbpopulator/populator/dbpopulator.go
@@ -1,4 +1,4 @@
-package dbpopulator
+package populator
 
 import (
 	"context"
@@ -15,52 +15,7 @@ type DBPopulator struct {
 	Repo *sqlc.Queries
 }
 
-//func CreateTrains() {
-//	req := rest.CreateTrainWithFullInfoRequest{}
-//	err := c.BodyParser(&req)
-//	if err != nil {
-//		return fiber.NewError(fiber.StatusBadRequest, "parse body error")
-//	}
-//
-//	trainID, err := t.Repo.CreateTrain(c.Context(), sqlc.CreateTrainParams{
-//		TrainName:       req.TrainName,
-//		StartStationID:  req.StartStation,
-//		DepartureTime:   req.DepartureTime,
-//		FinishStationID: req.FinishStation,
-//		ArrivalTime:     req.ArrivalTime,
-//	})
-//	if err != nil {
-//		return fiber.NewError(fiber.StatusInternalServerError, fmt.Sprintf("create train error: %v", err))
-//	}
-//	req.TrainID = trainID
-//	for i, car := range req.Cars {
-//		carID, err := t.Repo.CreateCar(c.Context(), sqlc.CreateCarParams{
-//			CarType: car.CarType,
-//			TrainID: trainID,
-//		})
-//		if err != nil {
-//			return fiber.NewError(fiber.StatusInternalServerError, "create car error")
-//		}
-//		req.Cars[i].CarID = carID
-//		for j, seat := range req.Cars[i].Seats {
-//			seatID, err := t.Repo.CreateSeat(c.Context(), sqlc.CreateSeatParams{
-//				CarID:       carID,
-//				SeatNumber:  seat.SeatNumber,
-//				IsAvailable: false,
-//				Price:       int32(seat.Price),
-//			})
-//			if err != nil {
-//				return fiber.NewError(fiber.StatusInternalServerError, "create seat error")
-//			}
-//			req.Cars[i].Seats[j].SeatID = seatID
-//		}
-//	}
-//
-//	return c.Status(fiber.StatusOK).JSON(req)
-//}
-
 func (p *DBPopulator) PopulateDB() {
-	train := rest.CreateTrainWithFullInfoRequest{}
 	stations := CreateStation(100)
 	for i, s := range stations {
 		id, err := p.Repo.CreateStation(context.Background(), s.StationName)
